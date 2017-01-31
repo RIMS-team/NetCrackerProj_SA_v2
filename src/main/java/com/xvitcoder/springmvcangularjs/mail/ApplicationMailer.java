e9385ca59392284743a07ddccf27837ca705ceb1
Seyone <seyone@mail.ru>
2017-01-31 20:45:55 +0200

    Add missed user id parameter to index.html

--- src/main/java/com/xvitcoder/springmvcangularjs/mail/ApplicationMailer.java
+++ src/main/java/com/xvitcoder/springmvcangularjs/mail/ApplicationMailer.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 public class ApplicationMailer {
 
